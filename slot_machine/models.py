294c0de76e6e535cb2d5b501bdad9b3b29aca960
Sarah Quigley <sarah@sarahquigley.net>
2015-12-12 20:49:18 -0500

    Create slot_machine app Word and Poem models.

--- slot_machine/models.py
+++ slot_machine/models.py
@@ -2,4 +2,20 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Word(models.Model):
+    word = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.word
+
+
+class Poem(models.Model):
+    text = models.TextField()
+    words = models.ManyToManyField(Word)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.text
