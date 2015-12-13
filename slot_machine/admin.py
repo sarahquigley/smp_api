b4bc9cda77e91c20a1ed807691c74877aea2cd13
Sarah Quigley <sarah@sarahquigley.net>
2015-12-12 20:50:13 -0500

    Add basic admin for slot_machine app Word and Post models.

--- slot_machine/admin.py
+++ slot_machine/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django import forms
+from slot_machine.models import Word, Poem
 
-# Register your models here.
+class PoemForm(forms.ModelForm):
+    words = forms.ModelMultipleChoiceField(queryset=Word.objects.all())
+
+class WordAdmin(admin.ModelAdmin):
+    list_display = ('word', 'created_at', 'modified_at')
+    readonly_fields = ('created_at', 'modified_at')
+
+class PoemAdmin(admin.ModelAdmin):
+    form = PoemForm
+    list_display = ('text', 'created_at', 'modified_at')
+    readonly_fields = ('created_at', 'modified_at')
+
+admin.site.register(Word, WordAdmin)
+admin.site.register(Poem, PoemAdmin)
