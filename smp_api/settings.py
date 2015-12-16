cf9ed9a919b12dc787c8fca570ffc482ba72331c
Sarah Quigley <sarah@sarahquigley.net>
2015-12-15 20:34:41 -0500

    Install corsheaders app and configure for development.

--- smp_api/settings.py
+++ smp_api/settings.py
@@ -40,12 +40,14 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_swagger',
     'debug_toolbar',
+    'corsheaders',
     'slot_machine',
 ]
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -123,3 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Corsheaders Config
+# https://github.com/ottoyiu/django-cors-headers/
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:9000',
+)
