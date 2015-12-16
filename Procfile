3954150dcd24499907092e967e90cb77bd099033
Sarah Quigley <sarah@sarahquigley.net>
2015-12-16 11:01:40 -0500

    Ready app for deployment on Heroku.
    - Add Procfile.
    - Update requirements for Heroku.
    - Split settings out for development and production.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn smp_api.wsgi --log-file -
---
+++ smp_api/settings/production.py
@@ -0,0 +1,18 @@
+from smp_api.settings.base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+import os
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ['SECRET_KEY']
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
