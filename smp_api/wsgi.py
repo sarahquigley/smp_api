67e99e1180c5cffbb76bc7d34f62a9fcc79b5e6a
Sarah Quigley <sarah@sarahquigley.net>
2016-01-06 17:37:43 -0500

    Install and configure whitenoise.

--- smp_api/wsgi.py
+++ smp_api/wsgi.py
@@ -11,8 +11,13 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "smp_api.settings")
 
 application = Cling(get_wsgi_application())
 
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
